91a6a727e53424f20c55adf661c1121f3b7197d9
Rasib <rasibnadeem101@gmail.com>
2022-11-20 14:23:27 +0500

    Schema done need to do query now and auth setup

---
+++ prisma/constraints.sql
@@ -0,0 +1,21 @@
+/*
+model TagsOnBooks {
+  book_id String
+  book_tagId String
+  @@id([book_id, book_tagId])
+}
+
+model TagsOnVisualMedias {
+  book_id String
+  book_tagId String
+
+  @@id([book_id, book_tagId])
+}
+
+model TagsOnPodcasts {
+  book_id String
+  book_tagId String
+
+  @@id([book_id, book_tagId])
+}
+*/
