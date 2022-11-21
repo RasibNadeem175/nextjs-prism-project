0e4c4d6c599e0019c6c6119ddb50eba1a8474fb1
Rasib <rasibnadeem101@gmail.com>
2022-11-21 11:45:04 +0500

    working

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Post, { PostProps } from "../components/archive/Post"
 import prisma from '../common/prisma';
 
 //GetServerSideProps is used when you need to wait for api, GetStaticProps when your page is not ready for build time
+
 export const getStaticProps: GetStaticProps = async () => {
   const feed = await prisma.post.findMany({
     where: { published: true },
